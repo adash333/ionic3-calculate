02630f02f7f7b8d8439492640889cd47df7800ca
adash333 <adash333@hotmail.com>
2017-06-18 16:09:54 +0900

    initialize repository

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -7,8 +7,24 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  initValue: number;  // 元本入力欄と双方向データバインド
+  rate: number;       // 金利入力欄と双方向データバインド
 
+  constructor(public navCtrl: NavController) {
+  }
+
+  // 複利計算
+  calc(): number {
+    if (isNaN(this.initValue) || isNaN(this.rate)) {
+      // 元本または利率が数字でないときはnullを返す
+      return null;
+    }
+    let answer: number = this.initValue;
+    for (let i = 0; i < 10; i++) {
+      // 金利計算を10回繰り返して複利計算
+      answer = answer * (1 + this.rate / 100)
+    }
+    return Math.floor(answer);  // 計算結果を整数に変換
   }
 
 }
